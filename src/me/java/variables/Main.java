b2d5255df825cb5eedcfc50c06cd7357ad570060
Flimberkim <kws1111049@gmail.com>
2022-09-19 21:57:29 +0900

    Practice java, Variables

---
+++ src/me/java/variables/Main.java
@@ -0,0 +1,10 @@
+package me.java.variables;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a); // 현재 10의 값을 가지고 있는 변수
+        System.out.println('a'); // 문자 리터럴
+        System.out.println("a"); // 문자열 리터럴
+    }
+}
